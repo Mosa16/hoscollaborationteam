8641a6666fbe3f74769feced305ecddec0b01020
ProfM16 <profmosa@gmail.com>
2023-10-20 15:52:35 +0200

    #This website is later to be linked with the App.

---
+++ Websites/sowetoweb/www.sowetocityelectrical.co.za/Pages/Our.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.sowetocityelectrical.co.za.Pages
+{
+    public class OurModel : PageModel
+    {
+        private readonly ILogger<OurModel> _logger;
+
+        public OurModel(ILogger<OurModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
